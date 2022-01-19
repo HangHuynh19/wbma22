957f647f1cb5b8b6f84bd6e161959184f8ce575c
HangHuynh19 <48004616+HangHuynh19@users.noreply.github.com>
2022-01-19 13:43:16 +0200

    dummy login

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import {StatusBar} from 'react-native';
+import {MainProvider} from './contexts/MainContext';
 import Navigator from './navigators/Navigator';
 
 const App = () => {
   return (
     <>
+      <MainProvider>
+        <Navigator />
+      </MainProvider>
       <StatusBar barStyle="light-content" backgroundColor={'#61dafb'} />
-      <Navigator />
     </>
   );
 };
